5a671b0aea706d8d612ea8fec4d21dca4b288c20
Livia <liviafausto03@gmail.com>
2024-01-24 19:02:25 -0300

    fix: OpenAPI version

--- FlixSync/src/main/java/com/flixsync/FlixSync/FlixSyncApplication.java
+++ FlixSync/src/main/java/com/flixsync/FlixSync/FlixSyncApplication.java
@@ -1,9 +1,12 @@
 package com.flixsync.FlixSync;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "FlixSync", description = "FlixSync documentation", version = "2.3.0"))
 public class FlixSyncApplication {
 
 	public static void main(String[] args) {

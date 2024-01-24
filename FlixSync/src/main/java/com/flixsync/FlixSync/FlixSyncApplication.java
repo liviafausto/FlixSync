dd2f581547f3f129117967edf52947ddb84d2ed1
Livia <liviafausto03@gmail.com>
2024-01-23 22:21:47 -0300

    add: project files

---
+++ FlixSync/src/main/java/com/flixsync/FlixSync/FlixSyncApplication.java
@@ -0,0 +1,13 @@
+package com.flixsync.FlixSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixSyncApplication.class, args);
+	}
+
+}
---
+++ FlixSync/src/test/java/com/flixsync/FlixSync/FlixSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flixsync.FlixSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1289a3864ee115b5631206c46e0f49385e5d05f5
Livia <liviafausto03@gmail.com>
2024-03-04 19:49:02 -0300

    fix: OpenAPI definition

--- FlixSync/src/main/java/com/flixsync/FlixSyncApplication.java
+++ FlixSync/src/main/java/com/flixsync/FlixSyncApplication.java
@@ -1,12 +1,16 @@
-package com.flixsync.FlixSync;
+package com.flixsync;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "FlixSync", description = "FlixSync documentation", version = "2.3.0"))
+@OpenAPIDefinition(
+		info = @Info(title = "FlixSync", description = "OpenApi documentation for FlixSync", version = "v1.0.0"),
+		servers = @Server(url = "http://localhost:8080", description = "LOCAL ENVIRONMENT")
+)
 public class FlixSyncApplication {
 
 	public static void main(String[] args) {

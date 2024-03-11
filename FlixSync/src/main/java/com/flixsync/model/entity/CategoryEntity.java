48ff482084b1254fab8a2b9d557ffabbdb93f6a7
Livia <liviafausto03@gmail.com>
2024-03-11 11:42:55 -0300

    feat: category model

---
+++ FlixSync/src/main/java/com/flixsync/model/entity/CategoryEntity.java
@@ -0,0 +1,23 @@
+package com.flixsync.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Category")
+@Table(name = "Category")
+public class CategoryEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+}

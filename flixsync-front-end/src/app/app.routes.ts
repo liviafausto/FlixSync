b085e24671728ec70e78e045db7a86461df73b45
Livia <liviafausto03@gmail.com>
2024-07-30 16:46:19 -0300

    feat: movies basic module

--- flixsync-front-end/src/app/app.routes.ts
+++ flixsync-front-end/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'movies',
+    loadChildren: () => import('./movies/movies.module').then(m => m.MoviesModule)
+  }
+];

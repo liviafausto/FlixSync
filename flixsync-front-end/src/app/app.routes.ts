f3f5b9af8d0fddda64f9b9e11eb406a92a032060
Livia <liviafausto03@gmail.com>
2024-08-01 13:41:37 -0300

    feat: temporary redirect to movies

--- flixsync-front-end/src/app/app.routes.ts
+++ flixsync-front-end/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full',  redirectTo: 'movies'},
   {
     path: 'movies',
     loadChildren: () => import('./movies/movies.module').then(m => m.MoviesModule)
